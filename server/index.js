1cda7e071c145bbb744cede6236858cca4946bb9
Brady Pitts <this.is.bp67c@gmail.com>
2021-09-02 10:08:48 -0600

    fixes

--- server/index.js
+++ server/index.js
@@ -53,10 +53,11 @@ app.put('/api/products', productController.addProduct);
 // app.get('/api/wishlist', productController.getWishlist);
 // app.post('/api/wishlist', productController.addToWishlist);
 
-app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`))
 
 app.use(express.static(`${__dirname}/../build`));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'))
 })
+
+app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`))
