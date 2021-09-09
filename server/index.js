98422ba4d38bd68e08098a66d38535485967e600
Brady Pitts <this.is.bp67c@gmail.com>
2021-09-09 16:45:43 -0600

    more stuff

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path: __dirname + '/../.env'});
 const express = require('express');
 const session = require('express-session');
 const massive = require('massive');
