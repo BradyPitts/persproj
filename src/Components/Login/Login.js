6d6edd0456b3b55fce6834e44245c0ed758538fc
Brady Pitts <this.is.bp67c@gmail.com>
2021-10-12 14:42:06 -0600

    nodemailer installed and begun

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,56 +2,93 @@ import React, { Component } from 'react';
 // import Background from '../shared/Background/Background'
 // import axios from 'axios';
 import {connect} from 'react-redux';
-import {login, signUp, logout} from '../../redux/userReducer';
+import {login, signUp, logout, sendNewPassword} from '../../redux/userReducer';
 import './Login.css';
+const nodemailer = require('nodemailer');
+
 
 
 class Login extends Component {
+  constructor(){
+    super();
 
-    handleEmail(value) {
-        console.log(value);
-        this.setState({ email: value });
-      }
-    
-      handlePassword(value) {
-        console.log(value);
-        this.setState({ password: value });
-      }
-
-    render() {
-      console.log(this.props.user)
-        return (
-            <div id='login' > 
-                {this.props.user.isLoggedIn ? 
-
-                  <div id='logout'>
-                  <button onClick={() => this.props.logout()} >Log out</button>
-                  </div>
-                  :
-                  <div id='log-in'>
-
-                    <div className="form-group">
-                        <label>Email</label>
-                        <input type="email" className="form-control" placeholder="Email" onChange={e => this.handleEmail(e.target.value)} />
-                    </div>
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Password" onChange={e => this.handlePassword(e.target.value)} />
-                    </div>
-
-                    <div id='buttons'>
-                      <button onClick={() => this.props.login(this.state.email, this.state.password)}>Log In</button>
-                      <button onClick={() => this.props.signUp(this.state.email, this.state.password, this.props.user.admin)}>Sign Up</button>
-                      <button onClick={() => this.props.signUp(this.state.email, this.state.password, this.props.user.admin)}>Forgot Password</button>
-                    </div>
-                  </div>
-
-                }
-                    
-            </div>
-        )
+      let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          type: 'OAuth2',
+          user: process.env.MAIL_USERNAME,
+          pass: process.env.MAIL_PASSWORD,
+          clientId: process.env.OAUTH_CLIENTID,
+          clientSecret: process.env.OAUTH_CLIENT_SECRET,
+          refreshToken: process.env.OAUTH_REFRESH_TOKEN
+        }
+      });
+      
+      let mailOptions = {
+        from: process.env.MAIL_USERNAME,
+        to: process.env.MAIL_USERNAME,
+        subject: 'nodemailer test',
+        text:'hello'
+      };
+      
+      transporter.sendMail(mailOptions, function(err, data){
+        if (err) {
+          console.log('Error' + err); 
+        } else {
+          console.log('Email sent')
+        }
+      })
+      
+  }
+      
+  handleEmail(value) {
+      console.log(value);
+      this.setState({ email: value });
     }
-}
+  
+  handlePassword(value) {
+    console.log(value);
+    this.setState({ password: value });
+  }
+
+ 
+
+
+
+  render() {
+    console.log(this.props.user)
+      return (
+        <div id='login' > 
+          {this.props.user.isLoggedIn ? 
+
+            <div id='logout'>
+              <button onClick={() => this.props.logout()} >Log out</button>
+            </div>
+            :
+            <div id='log-in'>
+
+              <div className="form-group">
+                <label>Email</label>
+                <input type="email" className="form-control" placeholder="Email" onChange={e => this.handleEmail(e.target.value)} />
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Password" onChange={e => this.handlePassword(e.target.value)} />
+              </div>
+
+              <div id='buttons'>
+                <button onClick={() => this.props.login(this.state.email, this.state.password)}>Log In</button>
+                <button onClick={() => this.props.signUp(this.state.email, this.state.password, this.props.user.admin)}>Sign Up</button>
+                <button onClick={() => this.transporter.sendMail()}>Forgot Password</button>
+              </div>
+            </div>
+
+          }
+                
+        </div>
+      )
+    }
+  }
 
 function mapStateToProps(state){
     return {
--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 const initialState = {
   user_id: null,
   email: '',
@@ -7,7 +8,9 @@ const initialState = {
   admin: false,
   isLoggedIn: false
   
-}
+};
+
+
 
 
 const REQUEST_USER_DATA = 'REQUEST_USER_DATA'
@@ -15,6 +18,7 @@ const REGISTER_USER = 'REGISTER_USER'
 const LOGIN_USER = 'LOGIN_USER'
 const LOGOUT_USER = 'LOGOUT_USER'
 const CONTINUE_AS_GUEST = 'CONTINUE_AS_GUEST'
+const SEND_NEW_PASSWORD = 'SEND_NEW_PASSWORD'
 
 
 export const requestUserData = () =>{
@@ -67,6 +71,14 @@ export const logout = () => {
   }
 };
 
+export const sendNewPassword = (email) => {
+  console.log(email)
+  return{
+    type: SEND_NEW_PASSWORD,
+    // payload: userData
+  }
+};
+
 
 export const continueAsGuest = () => {
   console.log(`continue pressed`)
@@ -80,16 +92,19 @@ export default function reducer(state = initialState, action){
       console.log(action.payload)
       return {...state, user: action.payload};
 
-      case REGISTER_USER + "_FULFILLED":
-        console.log(action.payload)
-        return{...state, user_id:action.payload.user.user_id, admin:action.payload.user.admin, isLoggedIn:action.payload.isLoggedIn}
+    case REGISTER_USER + "_FULFILLED":
+      console.log(action.payload)
+      return{...state, user_id:action.payload.user.user_id, admin:action.payload.user.admin, isLoggedIn:action.payload.isLoggedIn}
 
-      case LOGIN_USER + "_FULFILLED":
-        console.log(action.payload)
-        return{...state, user_id:action.payload.user.user_id, admin:action.payload.user.admin, isLoggedIn:action.payload.isLoggedIn}
+    case LOGIN_USER + "_FULFILLED":
+      console.log(action.payload)
+      return{...state, user_id:action.payload.user.user_id, admin:action.payload.user.admin, isLoggedIn:action.payload.isLoggedIn}
 
-        case LOGOUT_USER + "_FULFILLED":
-          return{initialState}
+    case LOGOUT_USER + "_FULFILLED":
+      return{initialState}
+
+    case SEND_NEW_PASSWORD + "_FULFILLED":
+      return{initialState}
 
     default:
     // console.log(state)
