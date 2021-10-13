fa1c100f711c8011e468091e8999ab3cbfaf09dc
Brady Pitts <this.is.bp67c@gmail.com>
2021-10-13 15:02:38 -0600

    more email auth changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -65,7 +65,8 @@ module.exports ={
         pass: process.env.MAIL_PASSWORD,
         clientId: process.env.OAUTH_CLIENTID,
         clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+        accessToken: process.env.OAUTH_ACCESS_TOKEN
       }
     });
     
